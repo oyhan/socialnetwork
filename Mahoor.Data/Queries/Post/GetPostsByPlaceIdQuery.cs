8fe83bc03d5f23c02d04851e1def6fe68652da3c
Ali Rahighi <rahighi@hotmail.com>
2021-07-27 11:58:02 +0430

    fixed series 3

---
+++ Mahoor.Data/Queries/Post/GetPostsByPlaceIdQuery.cs
@@ -0,0 +1,18 @@
+﻿using ChefCode.Common.Repository;
+using Mahoor.DomainObjects.Post;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mahoor.Data.Queries.Post
+{
+   public sealed class GetPostsByCityIdQuery : BaseSpecification<PostModel,Guid>
+    {
+        public GetPostsByCityIdQuery(Guid cityId)
+        {
+            AddCriteria(p => p.CityId == cityId);
+            ApplyOrderByDescending(p => p.CreatedDate);
+            ApplyPaging(0, 1);
+        }
+    }
+}
